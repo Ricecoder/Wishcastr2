dc1af26bcd1a2fb8c4b6267cfac61ce1ef2408cc
Lacey R <laceyrice@me.com>
2017-09-11 15:36:19 -0400

    Initial app.component commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  user: User;
+  userEndpoint: string = '/user';
+
+  constructor(private apiService: ApiService) { }
+  
+  getUser(): void {
+    this.user = this.apiService.getRequestUser(this.userEndpoint);    
+  }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
 }
