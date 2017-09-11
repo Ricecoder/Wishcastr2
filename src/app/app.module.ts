8ac274cc29632549b9db280535090ea3612a38db
Lacey R <laceyrice@me.com>
2017-09-11 15:36:53 -0400

    Initial app.module commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './results/results.component';
+import { UserWishesComponent } from './user-wishes/user-wishes.component';
+import { TopWishesComponent } from './top-wishes/top-wishes.component';
+import { WishComponent } from './wish/wish.component';
+import { SearchComponent } from './search/search.component';
+
+import { ApiService } from './api.service';
+import { RouterModule, Routes } from '@angular/router';
+import { LimitToPipe } from './limit-to.pipe';
+
+
+const appRoutes: Routes = [
+  { path: "results", component: ResultsComponent },
+  { path: "", component: TopWishesComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent,
+    UserWishesComponent,
+    TopWishesComponent,
+    WishComponent,
+    SearchComponent,
+    LimitToPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } 
+    ),
   ],
-  providers: [],
+  providers: [ ApiService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
