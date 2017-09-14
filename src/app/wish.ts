b3b15ff37f7796f988912eea0efa04e6c6dc8109
Lacey R <laceyrice@me.com>
2017-09-14 12:40:13 -0400

    Removed unnecessary code

--- src/app/wish.ts
+++ src/app/wish.ts
@@ -12,15 +12,3 @@ export class Wish {
         public type: string,
     ) {}
 }
-
-
-// affiliate_url:"https://www.amazon.com/Nintendo-New-3DS-XL-Galaxy-Style/dp/B01KOFZOYW?psc=1&SubscriptionId=AKIAISFMWONIHJJHH5TA&tag=wishcastr-20&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B01KOFZOYW"
-// brand:"Nintendo"
-// current_price:199
-// description:"Galaxy↵The New Nintendo 3DS XL system plays all Nintendo DS games. Nintendo DS games will not appear in 3D↵AC Adapter sold separately. New Nintendo 3DS XL uses the same AC adapter as Nintendo DSi, Nintendo 3DS and 2DS.↵3D Mode recommended for Ages 7+"
-// id:null
-// image_large:"https://images-na.ssl-images-amazon.com/images/I/51lev59IEGL.jpg"
-// image_thumbnail:"https://images-na.ssl-images-amazon.com/images/I/31KKKk0K67L._SL110_.jpg"
-// sku:"B01KOFZOYW"
-// title:"Nintendo New 3 Ds Xl   Galaxy Style"
-// type:"AmazonProduct"
